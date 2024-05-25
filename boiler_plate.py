6705306326d02013015aec2cc1cd929144abbd1b
aadium <aadi.p.um@gmail.com>
2024-05-25 11:44:22 -0400

    Rectified function declarations

--- boiler_plate.py
+++ boiler_plate.py
@@ -24,7 +24,7 @@ print("running main")
 #add starter functions here
 
 #rclpy,spin_once is a function that updates the ros topics once
-rclpy.spin_once(robot, timeout_sec=0.1)
+rclpy.spin_once(timeout_sec=0.1)
 
 #run control functions on loop
 try:
@@ -32,7 +32,7 @@ try:
     while True:
 
         #rclpy,spin_once is a function that updates the ros topics once
-        rclpy.spin_once(robot, timeout_sec=0.1)
+        rclpy.spin_once(timeout_sec=0.1)
 
         #Add looping functionality here
 
@@ -42,10 +42,10 @@ try:
         scan = robot.checkScan()
 
         # # If robot gets too close to wall -> moves backward
-        if lidar_data_too_close(robot, scan, np.pi/4, 3(*np.pi)/4, 0.1) > 0.5:
-            move_backward(robot)
+        if robot.lidar_data_too_close(scan, np.pi/4, 3(*np.pi)/4, 0.1) > 0.5:
+            robot.move_backward()
         else: # # If not too close to wall
-            move_forward(robot)
+            robot.move_forward()
 
 except KeyboardInterrupt:
     print("keyboard interrupt receieved.Stopping...")
