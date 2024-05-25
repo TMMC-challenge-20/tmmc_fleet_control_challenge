73d833dac71d2b5adcb6f1497dd6f8ef8ef79697
marrob0 <mroberto@uwaterloo.ca>
2024-05-25 11:28:08 -0400

    ^

--- boiler_plate.py
+++ boiler_plate.py
@@ -42,6 +42,8 @@ try:
         # # If robot gets too close to wall -> moves backward
         if lidar_data_too_close(robot, scan, np.pi/4, 3(*np.pi)/4, 0.1) > 0.5:
             move_backward(robot)
+        else: # # If not too close to wall
+            move_forward(robot)
 
 except KeyboardInterrupt:
     print("keyboard interrupt receieved.Stopping...")
